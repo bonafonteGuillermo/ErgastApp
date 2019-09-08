ecf6bd2e445eb599a2f1943834cf4fdac64338a6
Guillermo Bonafonte Criado <guillermo.bonafonte@nomasystems.com>
2019-09-08 15:13:12 +0200

    Removed filter

--- DriverList.js
+++ DriverList.js
@@ -1,24 +1,13 @@
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
   FlatList,
-  Text,
-  Modal,
   Image,
-  Alert,
   Picker,
-  ListHeaderComponent,
   View,
   TouchableWithoutFeedback
 } from 'react-native';
 
-import {
-  DialogComponent,
-  SlideAnimation,
-  DialogButton,
-  DialogContent,DialogTitle
-} from "react-native-dialog-component";
 import DriverRow from './DriverRow'
 import ApiClient from './ApiClient'
 
@@ -29,8 +18,6 @@ export default class DriverList extends Component {
                    this.apiClient = new ApiClient();
                    this.state = {
                      drivers: [],
-                     searchYearParam: "2018",
-                     filterItems: ["a", "b"]
                    };
                  }
 
@@ -40,55 +27,19 @@ export default class DriverList extends Component {
                      title: "F1 Standings App",
                      headerTintColor: "white",
                      headerStyle: { backgroundColor: "#a37d00" },
-                     headerRight: (
-                       <TouchableWithoutFeedback
-                         onPress={navigation.getParam("filterButtonPressed")}
-                       >
-                         <Image
-                           style={{ width: 24, height: 24, marginEnd: 10 }}
-                           source={require("./assets/calendar.png")}
-                         />
-                       </TouchableWithoutFeedback>
-                     )
                    };
                  };
 
                  componentWillMount() {
                    this.loadContent();
-                   this.initSearchFilterItems();
-                 }
-
-                 componentDidMount() {
-                   this.props.navigation.setParams({
-                     filterButtonPressed: this._filterButtonPressed
-                   });
                  }
 
                  loadContent = async () => {
-                   const drivers = await this.apiClient.getDriverStandings(
-                     2018
-                   );
+                   const drivers = await this.apiClient.getDriverStandings(2019);
                    this.setState({ drivers: drivers });
                  };
 
-                 initSearchFilterItems() {
-                   let searchFilterItems = [];
-                   for (let year = 1950; year < 2019; year++) {
-                     searchFilterItems.push(year);
-                   }
-                   this.setState({filterItems: searchFilterItems})
-                 }
-
-                 _filterButtonPressed = () => {
-                   this.dialogComponent.show();
-                 };
-
                  render() {
-                   let searchFilterItems = this.state.filterItems.map(
-                     (it, index) => {
-                       return <Picker.Item key={index} value={it} label={it} />;
-                     }
-                   );
                    return (
                      <View style={styles.container}>
                        <FlatList
@@ -108,34 +59,7 @@ export default class DriverList extends Component {
                          keyExtractor={item => item.Driver.familyName}
                          ItemSeparatorComponent={this.renderSeparator}
                        />
-
-                       <DialogComponent
-                         ref={dialogComponent => {
-                           this.dialogComponent = dialogComponent;
-                         }}
-                         dialogTitle={<DialogTitle title="Choose a year" />}
-                         dialogAnimation={
-                           new SlideAnimation({ slideFrom: "bottom" })
-                         }
-                         dismissOnTouchOutside={false}
-                         width="80%"
-                         height="50%"
-                       >
-                         <DialogContent style={(backgroundColor = "red")}>
-                           <View>
-                             <Picker
-                               selectedValue={this.state.language}
-                               style={{ height: 50, width: 100 }}
-                               onValueChange={(itemValue, itemIndex) =>
-                                 this.setState({ language: itemValue })
-                               }
-                             >
-                               {searchFilterItems}
-                             </Picker>
-                           </View>
-                         </DialogContent>
-                       </DialogComponent>
-                     </View>
+                </View>
                    );
                  }
 

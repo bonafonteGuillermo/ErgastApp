364cdfad77c1634ebd8205070ad36c8f37fc6116
Guillermo Bonafonte Criado <guillermo.bonafonte@nomasystems.com>
2019-09-09 22:55:34 +0200

    Removed code smell

--- LocalStorageManager.js
+++ LocalStorageManager.js
@@ -5,19 +5,19 @@ export default class LocalStorageManager {
     saveDriverInLocalStorage = async (driverId) => {
         const res = await AsyncStorage.setItem(driverId, 'true').catch(error => {
             console.log('saveDriverInLocalStorage'+error);
-          })
+            })
         return res;
     }
 
     removeDriverFromLocalStorage = async (driverId) => {
         return await AsyncStorage.removeItem(driverId).catch(error => {
             console.log('removeDriverFromLocalStorage'+error);
-          })
+            })
     }
 
     isDriverSavedLocalStorage = async (driverId) => {
         return await AsyncStorage.getItem(driverId).catch(error => {
             console.log('isDriverSavedLocalStorage'+error);
-          });
+            });
     }
 }
